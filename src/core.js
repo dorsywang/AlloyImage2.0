f8eabdf3cae196f071c0918885f1ad52b11045c6
dorsywang <honghu91@hotmail.com>
2016-10-23 22:10:30 +0800

    nint

--- src/core.js
+++ src/core.js
@@ -1,12 +1,53 @@
+class Tasker{
+    constructor(func){
+        this.__ = [];
+
+        this._state = 'finished';
+
+    }
+    push(item, func){
+        this.__.push([item, func]);
+
+        this.nextTick();
+    }
+
+    nextTick(){
+        if(this._state === 'finished'){
+
+            let first = this.__.shift();
+
+            if(first){
+                let operArgs = first[0];
+                let operFunc = first[1];
+
+                operFunc(operArgs.args).then(data => {
+                    this.nextTick();
+                }, data => {
+                });;
+
+                this._state = 'pending';
+            }else{
+            }
+
+        }else{
+        }
+    }
+}
+
 class AlloyImage{
     constructor(){
+        this.taskQueue = new Tasker((item) => {
+        });
     }
 
     act(...args){
+        this.taskQueue.push({
+            method: 'act',
+            args: args
+        }, (args){
+        });
     }
 
-    addTask(method, args){
-    }
 
     next(){
     }
